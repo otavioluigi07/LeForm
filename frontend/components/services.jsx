a72ebe33754a74f2c284e696db458aaf4339791c
Luigi Otávio <123912314+otavioluigi07@users.noreply.github.com>
2025-04-01 22:18:27 -0300

    ajustes banner e Services

---
+++ frontend/components/services.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import icon1 from "../public/assets/icon1.png"
+
+function Services() {
+  return (
+    <div className="">
+      <div>
+        <h2 className='text-bold'>Serviços</h2>
+      </div>
+
+      <div className='mt-16'>
+
+        <div className='flex items-start'>
+          <img src={icon1} alt="Icone" className='mr-4 mt-4' />
+          <div>
+            <h2 className=' font-bolt mb-2'>Cirurgia Plástica Facial</h2>
+            <p className='mb-16'>Harmonização e rejuvenescimento para realçar sua beleza natural.</p>
+            <div className='border-b-2 border-gray-400 mb-12'></div>
+          </div>
+        </div>
+        <div className='flex items-start'>
+          <img src={icon1} alt="Icone" className='mr-4 mt-4' />
+          <div>
+            <h2 className='mb-2'>Cirurgia Plástica Facial</h2>
+            <p className='mb-16'>Harmonização e rejuvenescimento para realçar sua beleza natural.</p>
+            <div className='border-b-2 border-gray-400 mb-12'></div>
+          </div>
+        </div>
+        <div className='flex items-start'>
+          <img src={icon1} alt="Icone" className='mr-4 mt-4' />
+          <div>
+            <h2 className='mb-2'>Cirurgia Plástica Facial</h2>
+            <p className='mb-16'>Harmonização e rejuvenescimento para realçar sua beleza natural.</p>
+            <div className='border-b-2 border-gray-400 mb-12'></div>
+          </div>
+        </div>
+        
+        
+
+        
+
+      </div>
+    </div>
+  )
+}
+
+export default Services
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Navbar from '../components/navbar' 
 import Banner from '../components/banner'
 import './App.css'
+import Services from '../components/services'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
     <div className='px-10'>
       <Navbar></Navbar>
       <Banner></Banner>
+      <Services></Services>
 
 
 
